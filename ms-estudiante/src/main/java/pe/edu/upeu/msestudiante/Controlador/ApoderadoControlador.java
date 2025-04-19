8bc629b73d94aaaa51eb8554babca853cab83cc3
paric <paric@kenland>
2025-04-19 18:31:08 -0500

    HOLAKR5

--- ms-estudiante/src/main/java/pe/edu/upeu/msestudiante/Controlador/ApoderadoControlador.java
+++ ms-estudiante/src/main/java/pe/edu/upeu/msestudiante/Controlador/ApoderadoControlador.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/apoderado")
+@RequestMapping("/apoderados")
 public class ApoderadoControlador {
 
     @Autowired
--- ms-estudiante/src/main/java/pe/edu/upeu/msestudiante/Controlador/EstudianteControlador.java
+++ ms-estudiante/src/main/java/pe/edu/upeu/msestudiante/Controlador/EstudianteControlador.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/estudiante")
+@RequestMapping("/estudiantes")
 public class EstudianteControlador {
     @Autowired
     private EstudianteServicio estudianteServicio;
